876670754589addab207da313f69a85ea9a6399c
Talles Pellense <160547176+tallesSENAI@users.noreply.github.com>
2025-01-28 09:34:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const cors = require ('cors');
+const meuServidor = express();
+
+meuServidor.use(express.json());
+meuServidor.use(cors());
+
+//------------------------------------------------------------
+const rotasCadastroUsuario = require('./Usuario/UsuarioCadastro/usuarioCadastroControlador');
+meuServidor.use(rotasCadastroUsuario);
+
+const rotasLoginUsuario = require('./Usuario/UsuarioLogin/usuarioLoginControlador');
+meuServidor.use(rotasLoginUsuario);
+
+const rotasAnuncioCarro = require('./AnuncioCarro/anuncioCarroControlador');
+meuServidor.use(rotasAnuncioCarro);
+
+const rotasAnuncioPeca = require('./AnuncioPeca/anuncioPecaControlador');
+meuServidor.use(rotasAnuncioPeca);
+
+// const rotasLoginUsuario = require('./Usuario/UsuarioLogin/usuarioLoginControlador');
+// app.use(rotasLoginUsuario); (EM ANÁLISE PARA VER SE REALMENTE UMA ROTA PARA LOGIN É PRECISO)
+
+
+
+
+
+
+
+
+//----------------------------------------------------------------------------------
+// //'cors' controla o acesso ao servidor
+
+// app.use(express.json());
+// 
+// //----------------------------------------------------------------------------------
+
+// //ROTAS USUÁRIOS
+// app.get('/usuarios', (req, res) => { //get
+//     res.json('Usuário selecionado');
+// });
+
+// app.post('/usuarios', (req, res) => { //post
+//     res.json('Usuário CADASTRADO com sucesso!');
+// });
+
+// app.delete('/usuarios', (req, res) => { //delete
+//     res.json('Usuário DELETADO com sucesso!');
+// });
+// //----------------------------------------------------------------------------------
+
+// //ROTAS ANUNCIOS
+// app.get('/anuncios', (req, res) => { //get
+//     res.json('Anúncio selecionado.');
+// });
+
+// app.post('/anuncios', (req, res) => { //post
+//     res.json('Anúncio POSTADO com sucesso!');
+// });
+
+// app.delete('/anuncios', (req, res) => { //delete
+//     res.json('Anúncio DELETADO com sucesso!');
+// });
+
+// //----------------------------------------------------------------------------------
+meuServidor.listen(4300, () => {
+    console.log('Conexão com arquivo INDEX bem sucedida!');
+});
